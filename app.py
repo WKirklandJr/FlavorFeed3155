b1b5d581b931c79ab4ca719d084f7f2c0412ed2e
Zachary Ellixson <intrilogies@gmail.com>
2023-03-29 00:01:45 -0400

    Adding app and template routes

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = (__name__)
+
+@app.get('/recipes')
+def recipes():
+    return()
+
+@app.get('/recipes/<id>')
+def get_recipe():
+    return()
+
+@app.get('/recipes/new')
+def new_recipe():
+    return()
+
+@app.post('/recipes')
+def create_recipe():
+    return()
+
+@app.get('/recipes/<id>/edit')
+def edit_recipe():
+    return()
+
+@app.post('/recipes/<id>')
+def update_recipe():
+    return()
+
+@app.post('/recipes/<id>/delete')
+def delete_recipe():
+    return()

82edaf8d9b6b10f0fc7c0a8cb93af79626da97fb
jikingg <jaskin233@gmail.com>
2023-04-11 22:25:51 -0400

    Creating new template pages

--- app.py
+++ app.py
@@ -25,6 +25,16 @@ db.init_app(app)
 def index():
     return render_template('index.html')
 
+
+#LOGIN PAGES
+def login():
+    return render_template('login.html')
+
+def register():
+    return render_template('registration.html')
+
+#RECIPES PAGES
+
 @app.get('/recipes')
 def recipes():
     all_recipes = recipe_repository_singleton.get_all_recipes()

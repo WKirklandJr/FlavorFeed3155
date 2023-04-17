772e469a993ef0631766ec3a515c507cc530fb22
jikingg <jaskin233@gmail.com>
2023-04-17 02:18:28 -0400

    update index.html

--- recipes_schema.sql
+++ recipes_schema.sql
@@ -10,3 +10,14 @@ text TEXT NOT NULL,
 PRIMARY KEY (recipe_id)
 );
 
+-- Create user table (updated as new varibles are implemented)
+CREATE TABLE IF NOT EXISTS users (
+user_id SERIAL NOT NULL,
+user_name VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+user_image BYTEA,
+skill_level VARCHAR(255) NOT NULL,
+PRIMARY KEY (user_id)
+);
+

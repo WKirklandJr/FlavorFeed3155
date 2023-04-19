d909db1db878c2022e4646f1fd0c46454d54ff33
Zachary Ellixson <intrilogies@gmail.com>
2023-04-19 18:40:17 -0400

    Adding login and logout features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, abort
+from flask import Flask, session, redirect, render_template, request, abort
 from dotenv import load_dotenv
 import os
 
@@ -20,6 +20,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] \
     = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.secret_key = os.getenv('APP_SECRET')
+
 db.init_app(app)
 bcrypt.init_app(app)
 
@@ -36,13 +38,63 @@ def about():
 
 #LOGIN PAGES
 @app.get('/login')
-def login():
+def login_form():
+    if 'user' in session:
+        return redirect('/user/profile')
+    
     return render_template('login.html')
+
+@app.post('/login')
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        abort(400)
+
+    existing_user = User.query.filter_by(username=username).first()
+
+    if not existing_user:
+        return redirect('/login')
+    
+    if not bcrypt.check_password_hash(existing_user.password, password):
+        return redirect('/login')
+    
+    session['user'] = {
+        'username': username
+    }
+
+    return redirect('/user/profile')
 
 @app.get('/register')
 def register():
+    if 'user' in session:
+        return redirect('user/profile')
+    
     return render_template('registration.html')
 
+@app.post('/register')
+def signup():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not email or not username or not password:
+        abort(400)
+
+    hashed_password = bcrypt.generate_password_hash(password).decode()
+
+    new_user = User(email, username, hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/login')
+
+@app.get('/logout')
+def logout():
+    if 'user' in session:
+        del session['user']
+    return redirect('/login')
 
 #RECIPES PAGES
 
@@ -75,19 +127,11 @@ def delete_recipe():
     return()
 
 #POST PAGES
-@app.post('/register')
-def signup():
-    email = request.form.get('email')
-    username = request.form.get('username')
-    password = request.form.get('password')
 
-    if not email or not username or not password:
-        abort(400)
-
-    hashed_password = bcrypt.generate_password_hash(password).decode()
-
-    new_user = User(email, username, hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect('/login')
+# User pages
+@app.get('/user/profile')
+def profile():
+    if 'user' not in session:
+        return redirect('/login')
+    
+    return render_template('get_single_profile.html')

f5a408f85cfac4619d16c012b5b00d7704f1becd
Zachary Ellixson <intrilogies@gmail.com>
2023-04-26 20:14:55 -0400

    Adding tags, comments, and bookmarks

--- recipes_schema.sql
+++ recipes_schema.sql
@@ -1,5 +1,18 @@
+-- Create user table (updated as new varibles are implemented)
+CREATE TABLE IF NOT EXISTS users (
+user_id SERIAL NOT NULL,
+email VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
+"password" VARCHAR(255) NOT NULL,
+profile_picture VARCHAR(255) NOT NULL,
+skill VARCHAR(255) NOT NULL,
+social VARCHAR(255) NOT NULL,
+about TEXT NOT NULL,
+PRIMARY KEY (user_id)
+);
+
 -- Create recipes table (to be updated as variables are implemented)
-CREATE TABLE IF NOT EXISTS recipe (
+CREATE TABLE IF NOT EXISTS recipes (
 recipe_id SERIAL NOT NULL,
 title VARCHAR(255) NOT NULL,
 is_vegan BOOL  DEFAULT FALSE NOT NULL,
@@ -13,15 +26,37 @@ date_posted TIMESTAMP NOT NULL,
 PRIMARY KEY (recipe_id)
 );
 
--- Create user table (updated as new varibles are implemented)
-CREATE TABLE IF NOT EXISTS users (
-user_id SERIAL NOT NULL,
-email VARCHAR(255) NOT NULL,
-username VARCHAR(255) NOT NULL,
-"password" VARCHAR(255) NOT NULL,
-profile_picture VARCHAR(255) NOT NULL,
-skill VARCHAR(255) NOT NULL,
-social VARCHAR(255) NOT NULL,
-about TEXT NOT NULL,
-PRIMARY KEY (user_id)
+-- n:n relationship b/w users and recipes
+CREATE TABLE IF NOT EXISTS bookmarks (
+    user_id INT,
+    recipe_id INT,
+    PRIMARY KEY (user_id, recipe_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id)
+);
+
+-- table for tags
+CREATE TABLE IF NOT EXISTS tags (
+    tag_id SERIAL NOT NULL,
+    tag VARCHAR(255) NOT NULL,
+    PRIMARY KEY (tag_id)
+);
+
+-- n:n relationship b/w recipes and tags
+CREATE TABLE IF NOT EXISTS recipe_tag (
+    recipe_id INT,
+    tag_id INT,
+    PRIMARY KEY (recipe_id, tag_id),
+    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
+    FOREIGN KEY(tag_id) REFERENCES tags(tag_id)
+);
+
+-- n:n relationship b/w users and recipes
+CREATE TABLE IF NOT EXISTS user_recipe_comments (
+    user_id INT,
+    recipe_id INT,
+    comment TEXT NOT NULL,
+    PRIMARY KEY (user_id, recipe_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id)
 );
--- src/models.py
+++ src/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Table for users
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -23,7 +24,19 @@ class User(db.Model):
         self.social = ''
         self.about = ''
 
+# Junction table for the n:n relationship b/w users and recipes
+bookmarks = db.Table(
+    'bookmarks',
+    db.Column('user_id', db.Integer, \
+              db.ForeignKey('user.user_id'), primary_key=True),
+    db.Column('tag_id', db.Integer, \
+              db.ForeignKey('recipe.recipe_id'), primary_key=True)
+)
+
+# Table for recipes
 class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
     recipe_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     is_vegan = db.Column(db.Boolean, nullable=False)
@@ -34,11 +47,12 @@ class Recipe(db.Model):
     instructions = db.Column(db.String, nullable=False)
     recipe_image = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False)
-    #tags = db.Column(db.String, nullable=False)
 
-    # Users are not implemented yet
-    #author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    #author = db.relationship('User', backref='author')
+    author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    author = db.relationship('User', backref='author')
+
+    bookmark = db.relationship('User', secondary=bookmarks, backref='recipes')
+
 
     def __init__\
         (self, title,is_vegan,ingredients,equipment,duration,difficulty,instructions,recipe_image,date_posted) -> None:
@@ -51,4 +65,38 @@ class Recipe(db.Model):
         self.instructions = instructions
         self.recipe_image = recipe_image
         self.date_posted = date_posted
+
+class user_recipe_comment(db.Model):
+    __tablename__ = 'user_recipe_comments'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.recipe_id'), primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+
+    def __init__(self, user_id, recipe_id, comment):
+        self.user_id = user_id
+        self.recipe_id = recipe_id
+        self.comment = comment
+
+# Junction table for the n:n relationship b/w recipes and tags
+recipe_tag = db.Table(
+    'recipe_tag',
+    db.Column('recipe_id', db.Integer, \
+              db.ForeignKey('recipe.recipe_id'), primary_key=True),
+    db.Column('tag_id', db.Integer, \
+              db.ForeignKey('tag.tag_id'), primary_key=True)
+)
+
+# Table for tags
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    tag_id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String, nullable=False)
+
+    recipes = db.relationship('Recipe', secondary=recipe_tag, backref='tags')
+
+    def __init__(self, tag_id, tag):
+        self.tag_id = tag_id
+        self.tag = tag
 

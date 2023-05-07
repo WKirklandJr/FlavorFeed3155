43ec404c5e10846142e649dab46f7a6b7bed08f8
jikingg <jaskin233@gmail.com>
2023-05-06 22:18:43 -0400

    c

--- src/repositories/tags_repository.py
+++ src/repositories/tags_repository.py
@@ -2,13 +2,6 @@ from src.models import db, Tag
 
 class TagRepository:
 
-    def create_tag(self, tagname):
-        create_tag = Tag(tagname)
-
-        db.session.add(create_tag)
-        db.session.commit()
-
-        return create_tag
     
     def get_tag(self, tagname):
         tag_name = Tag.query.filter_by(tagname=tagname).first_or_404()

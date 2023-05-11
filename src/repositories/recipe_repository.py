7e710b9135bc148a6eda148bb66624799da954e2
jikingg <jaskin233@gmail.com>
2023-05-10 22:20:36 -0400

    cleaning up routers and templates

--- src/repositories/recipe_repository.py
+++ src/repositories/recipe_repository.py
@@ -14,15 +14,30 @@ class RecipeRepository:
     def filter_recipe_by_id(self, recipe_id):
         get_recipe = Recipe.query.filter_by(recipe_id = recipe_id).first()
         return get_recipe
-
+    
     def get_recipes_by_user(self, user_id):
         get_recipe = Recipe.query.filter_by(user_id = user_id).all()
+        return get_recipe
+    
+    def get_recipe_by_difficulty(self, difficulty):
+        get_recipe = Recipe.query.filter_by(difficulty = difficulty).all()
+        return get_recipe
+    
+
+    def get_recipe_by_vegan(self, isvegan):
+        get_recipe = Recipe.query.filter_by(is_vegan = isvegan).all()
+        return get_recipe
+    
+    
+    def get_recipe_by_duration(self, duration):
+        get_recipe = Recipe.query.filter(Recipe.duration < duration).all()
         return get_recipe
 
 
     def get_recipes_by_bookmark(self, user_id):
         get_recipe = Recipe.query.join(Recipe.bookmark).filter_by(user_id = user_id).all()
         return get_recipe
+    
 
     def create_recipe(self, title, is_vegan, ingredients, equipment, duration, difficulty, instructions, recipe_image, date_posted, user_id):
         
@@ -52,6 +67,10 @@ class RecipeRepository:
         db.session.delete(recipe_to_delete)
         db.session.commit()
         return recipe_to_delete
+    
+    def search_recipe(self, title):
+        found_recipes = Recipe.query.filter(Recipe.title.ilike(f'%{title}%')).all()
+        return found_recipes
 
 # Singleton: Restricts the instantiation of a class to a single instance
 recipe_repository_singleton = RecipeRepository()

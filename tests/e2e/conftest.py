a40a20cb8f4d5040f952cd420e4f5646012bc35c
Jose Lara-Urbina <joselaraurb@gmail.com>
2023-05-11 16:24:13 -0400

    Refreshing db when tests are ran

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from app import app
+from src.models import db, User, Recipe, Tag, Bookmark, Comment
 
 @pytest.fixture(scope='module')
 def test_app():
-    return app.test_client()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        yield app.test_client()
+        db.drop_all()

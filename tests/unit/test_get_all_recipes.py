1ade894b21265ce0dd91a4b75eb2086c15101789
Jose Lara-Urbina <joselaraurb@gmail.com>
2023-05-11 20:09:12 -0400

    finish implemention of unit tests

--- tests/unit/test_get_all_recipes.py
+++ tests/unit/test_get_all_recipes.py
@@ -1,19 +1,23 @@
 from src.repositories.recipe_repository import recipe_repository_singleton
+from src.repositories.user_repository import user_repository_singleton
 from app import app
-from src.models import db, Recipe
+from src.models import db, Recipe, User
 
-def test_create_recipe():
+def test_get_all_users():
     with app.app_context():
         db.session.query(Recipe).delete()
+        db.session.query(User).delete()
         db.session.commit()
+
+        user = user_repository_singleton.create_user('test@example.com', 'testuser', 'password123')
 
         recipe = recipe_repository_singleton.create_recipe('test_recipe'\
             , False, 'chicken, rice, beans', 'pan, spatula', 30, 'Intermediate'\
-            , 'cook it', '.../static/post-images/chicken.png', '2023-05-11 15:42:12-07', '1')
+            , 'cook it', '.../static/post-images/chicken.png', '2023-05-11 15:42:12-07', user.user_id)
         
         recipe_2 = recipe_repository_singleton.create_recipe('test_recipe_2'\
             , False, 'chicken, rice, beans, broccoli', 'pan, spatula', 30, 'Intermediate'\
-            , 'cook it', '.../static/post-images/chicken.png', '2023-05-11 15:42:12-07', '1')
+            , 'cook it', '.../static/post-images/chicken.png', '2023-05-11 15:42:12-07', user.user_id)
         
         all_recipes = recipe_repository_singleton.get_all_recipes()
         

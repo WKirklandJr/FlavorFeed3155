af49351a541ed9a886f3b06517787daf54287c7f
jikingg <jaskin233@gmail.com>
2023-05-11 23:45:07 -0400

    finalizing

--- tests/e2e/test_get_single_recipe_page.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from src.repositories.recipe_repository import recipe_repository_singleton
-
-
-def test_get_single_recipe(client):
-     response = client.get("/recipes/1")
-     assert "<h2 class="">" in response.data
